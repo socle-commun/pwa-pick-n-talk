b38d40437ae8ab7bae2b33483791cb339d28f770
Michel Montejuado <michel.montejuado@gmail.com>
2025-05-24 11:36:36 +0200

    📦:  Add cn, headlessui and heroicons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import cn from "@/utilities/cn";
+
 function App() {
   return (
     <>
+      <h1 className={cn("text-3xl font-bold underline")}>Hello world!</h1>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,33 @@ import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/pick-n-talk/" : "/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react": ["react", "react-dom"],
+          "react-router": ["react-router"],
+          "cn": ["clsx", "tailwind-merge"],
+          "headlessui": ["@headlessui/react"],
+          "icons": ["@heroicons/react"]
+        }
+      }
+    },
+    minify: process.env.NODE_ENV === "production" ? "terser" : false,
+    terserOptions: {
+      compress: {
+        passes: 2
+      },
+      mangle: true,
+      format: {
+        comments: false
+      }
+    }
+  },
+  define: {
+    '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
+  },
   logLevel: "info",
   plugins: [react(), tailwindcss()],
   resolve: {
