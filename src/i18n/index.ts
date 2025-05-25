dd98d83d4d8ec2201147b476fef537db5e613b66
Michel Montejuado <michel.montejuado@gmail.com>
2025-05-25 09:27:18 +0200

    ✨: Initialise i18n

---
+++ src/i18n/index.ts
@@ -0,0 +1,22 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+import { initReactI18next } from "react-i18next";
+
+i18n
+	// load translation using http -> see /public/locales
+	// learn more: https://github.com/i18next/i18next-http-backend
+	.use(Backend)
+	// detect user language
+	// learn more: https://github.com/i18next/i18next-browser-languageDetector
+	.use(LanguageDetector)
+	.use(initReactI18next) // passes i18n down to react-i18next
+	.init({
+		supportedLngs: ["fr-FR", "en-US"],
+		fallbackLng: ["fr-FR", "en-US"],
+		interpolation: {
+			escapeValue: false // react already safes from xss
+		}
+	});
+
+export default i18n;
